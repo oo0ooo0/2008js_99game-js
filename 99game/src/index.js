d2e14b88729df420578d21ac22a1f1341a49fbcb
oo0ooo0 <nyanyanyangnyang@gmailcom>
2020-08-18 07:13:57 +0900

    99game ver2(화면 구성)

--- 99game/src/index.js
+++ 99game/src/index.js
@@ -1,17 +1,58 @@
 // import "./styles.css";
 
-while (true) {
-  var 숫자1 = Math.floor(Math.random() * 9) + 1;
-  var 숫자2 = Math.floor(Math.random() * 9) + 1;
-  var 결과 = 숫자1 * 숫자2;
-  var 조건 = true;
-  while (조건) {
-    var 답 = prompt(String(숫자1) + '곱하기' + String(숫자2) + '는?');
-    if (결과 === Number(답)) {
-      alert('🤗𐐄');
-      조건 = false;
-    } else {
-      alert('땡');
-    }
+var num1 = Math.ceil(Math.random() * 9);
+var num2 = Math.ceil(Math.random() * 9);
+var result = num1 * num2;
+
+var body = document.body;
+var word = document.createElement('div');
+word.textContent = String(num1) + ' * ' + String(num2) + '= ?';
+document.body.append(word);
+
+//form box
+const formBox = document.createElement('form');
+document.body.append(formBox);
+
+//game input box
+const inputBox = document.createElement('input');
+// inputBox.type = 'number';  왜안되지 ㅇ ㅡㅇ ?
+formBox.append(inputBox);
+
+//complete answer button
+const btn = document.createElement('button');
+btn.textContent = '↪';
+formBox.append(btn);
+
+// print the answer check result
+const resultWindow = document.createElement('div');
+document.body.append(resultWindow);
+formBox.addEventListener('submit', function callBack(e) {
+  e.preventDefault();
+  // console.log(result, inputBox.value);
+  if (result === Number(inputBox.value)) {
+    resultWindow.textContent = '딩동댕 🤗 𐐄';
+    num1 = Math.ceil(Math.random() * 9);
+    num2 = Math.ceil(Math.random() * 9);
+    result = num1 * num2;
+    word.textContent = String(num1) + ' * ' + String(num2) + '= ?';
+
+    inputBox.value = '';
+    inputBox.focus();
+  } else {
+    resultWindow.textContent = '땡! 😱 X';
+    inputBox.value = '';
+    inputBox.focus();
   }
-}
+});
+
+// var 조건 = true;
+//   while (조건) {
+//     var 답 = prompt(String(숫자1) + '곱하기' + String(숫자2) + '는?');
+//     if (결과 === Number(답)) {
+//       alert('🤗𐐄');
+//       조건 = false;
+//     } else {
+//       alert('땡');
+//     }
+//   }
+// }
