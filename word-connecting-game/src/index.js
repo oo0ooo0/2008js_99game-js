fc1a7b5a2b925a4ec626931011cb21f5bf3f44d8
oo0ooo0 <nyanyanyangnyang@gmailcom>
2020-08-17 13:57:14 +0900

    word connecting game ver2 (화면구성)

--- word-connecting-game/src/index.js
+++ word-connecting-game/src/index.js
@@ -1,5 +1,10 @@
 // import './styles.css';
 
+////////////////////
+//////////
+//ver1
+//////////
+////////////////////
 /*
 var word = 'game';
 while (true) {
@@ -14,14 +19,54 @@ while (true) {
 }
 */
 
-for (var word = 'game'; true; ) {
-  var answer = prompt(word);
-  if (word[word.length - 1] === answer[0]) {
-    //맞았을때
-    alert('딩동댕 🤗𐐄');
-    word = answer;
+////////////////////
+//////////
+//ver2
+//////////
+////////////////////
+var body = document.body;
+
+document.title = 'word connecting game';
+
+//game title
+const gameTitle = document.createElement('h1');
+gameTitle.textContent = '끝말잇기 (word connecting game)';
+document.body.append(gameTitle);
+
+//game info
+const gameInfo = document.createElement('p');
+const infoKr = "'prompt'와 'if'문을 사용한 끝말잇기 게임.";
+const infoEn = "'A game of word connecting' is been made 'input' and 'while' statements.";
+gameInfo.innerHTML = infoKr + '<br />' + infoEn;
+document.body.append(gameInfo);
+
+//first connecting word
+var word = document.createElement('div');
+word.textContent = 'jamin';
+document.body.append(word);
+
+//game input box
+const inputBox = document.createElement('input');
+document.body.append(inputBox);
+
+//game complete answer button
+const btn = document.createElement('button');
+btn.textContent = '입력';
+document.body.append(btn);
+
+// print the answer check result
+const resultWindow = document.createElement('div');
+document.body.append(resultWindow);
+
+btn.addEventListener('click', function callBack() {
+  if (word.textContent[word.textContent.length - 1] === inputBox.value[0]) {
+    resultWindow.textContent = '딩동댕 🤗𐐄';
+    word.textContent = inputBox.value;
+    inputBox.value = '';
+    inputBox.focus();
   } else {
-    alert('땡! 😱X');
-    //틀렸을때
+    resultWindow.textContent = '땡! 😱X';
+    inputBox.value = '';
+    inputBox.focus();
   }
-}
+});
